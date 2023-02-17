40677587be5983431c0a08bd8cd8e79e05d5a3b1
nicolascalvo73 <84040201+nicolascalvo73@users.noreply.github.com>
2023-02-17 20:47:02 -0300

    Add files via upload

---
+++ 06-07-08.js
@@ -0,0 +1,90 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function crearClasePersona() {
+  class Persona {
+    constructor(nombre, edad, hobbies, amigos) {
+      // El constructor de la clase Persona recibe nombre (string), edad (integer), hobbies (array de strings), amigos (array de objetos)
+      // Inicializar las propiedades de la persona con los valores recibidos como argumento
+
+      // Tu código aca:
+      this.nombre = nombre
+      this.edad = edad
+      this.hobbies = hobbies
+      this.amigos = amigos
+    }
+
+    addFriend(nombre, edad) {
+      // El método 'addFriend' recibe un string 'nombre' y un entero 'edad' y debe agregar un objeto:
+      // { nombre: nombre, edad: edad} al arreglo de amigos de la persona.
+      // No debe retornar nada.
+
+      // Tu código aca:
+      this.amigos.push({ "nombre" : nombre, "edad" : edad })
+    }
+
+    addHobby(hobby) {
+      // El método 'addHobby' recibe un string 'hobby' y debe agregarlo al arreglo de hobbies de la persona.
+      // No debe retornar nada.
+
+      // Tu código aca:
+      this.hobbies.push(hobby)
+    }
+    getFriends() {
+      // El método 'getFriends' debe retornar un arreglo con sólo los nombres del arreglo de amigos
+      // de la persona.
+      // Ej:
+      // Suponiendo que la persona tiene estos amigos: [{nombre: 'martin', edad: 31},{nombre: 'toni', edad: 33}]
+      // persona.getFriends() debería devolver ['martin', 'toni']
+
+      // Tu código aca:
+      const friends = []
+      this.amigos.forEach( e => friends.push(e.nombre) )
+      return friends
+    }
+
+    getHobbies() {
+      // El método 'getHobbies' debe retornar un arreglo con los hobbies de la persona
+      // Ej:
+      // persona.getHobbies() debe devolver ['correr', 'dormir', 'nadar']
+
+      // Tu código aca:
+      const hobby = []
+      this.hobbies.forEach( e => hobby.push(e) )
+      console.log(hobby)
+      return hobby
+    }
+
+    getPromedioEdad() {
+      // El método 'getPromedioEdad' debe retornar el promedio de edad de los amigos de una persona
+      // Ej:
+      // Si la persona tuviera estos amigos:
+      // {
+      //   amigos: [{
+      //     nombre: 'toni',
+      //     edad: 33,
+      //   }, {
+      //     nombre: 'Emi',
+      //     edad: 25
+      //   }]
+      // }
+      // persona.getPromedioEdad() debería devolver 29 ya que (33 + 25) / 2 = 29
+
+      // Tu código aca:
+      const ages = []
+      this.amigos.forEach( e => ages.push(e.edad) )
+      const sum = ages.reduce((age1, age2) => age2 += age1)
+      const avg = sum/ages.length
+      return avg
+    }
+  };
+
+  return Persona;
+}
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = crearClasePersona
